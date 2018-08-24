83f6b87770d3c036ab12802baea35b10536e7648
Nazim Ali <nazim.ali@sitecore.com>
2018-08-24 16:46:04 -0400

    Create patch

---
+++ src/Sitecore.Support.91717/Checkout.cs
@@ -0,0 +1,38 @@
+﻿using Sitecore;
+using Sitecore.Shell.Framework.Commands;
+
+namespace Sitecore.Support.Shell.Framework.Commands
+{
+  public class Checkout : Sitecore.Shell.Framework.Commands.CheckOut
+  {
+    public override CommandState QueryState(CommandContext context)
+    {
+      if (context.Items.Length != 1)
+      {
+        return CommandState.Hidden;
+      }
+      if (Context.IsAdministrator)
+      {
+        return CommandState.Enabled;
+      }
+      var item = context.Items[0];
+      if (item.Locking.IsLocked())
+      {
+        return CommandState.Disabled;
+      }
+      if (item.Appearance.ReadOnly)
+      {
+        return CommandState.Disabled;
+      }
+      if (!item.Access.CanWrite())
+      {
+        return CommandState.Disabled;
+      }
+      if (item.Locking.HasLock())
+      {
+        return CommandState.Disabled;
+      }
+      return base.QueryState(context);
+    }
+  }
+}
---
+++ src/Sitecore.Support.91717/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.91717")]
+[assembly: AssemblyProduct("Sitecore.Support.91717")]
+[assembly: ComVisible(false)]
